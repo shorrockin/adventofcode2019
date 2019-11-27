2daf3731c632b6509e4e72cd7891ee5162c99b1a
Chris Shorrock <cs@stripe.com>
2019-11-27 15:54:50 -0800

    initial commit

---
+++ 01.rb
@@ -0,0 +1,23 @@
+require 'pry'
+require './boilerplate'
+
+class Solution < Boilerstate
+  def parse(input)
+  end
+
+  def part_one
+  end
+
+  def part_two
+  end
+end
+
+part 1 do
+  #assert_call_on(Solution.new("R2, L3"), 5, :part_one)
+  #log_call_on(Solution.new(input), :part_one)
+end
+
+part 2 do
+  #assert_call_on(Solution.new("R8, R4, R4, R8"), 4, :part_two)
+  #log_call_on(Solution.new(input), :part_two)
+end
---
+++ boilerplate.rb
@@ -0,0 +1,95 @@
+# Advent Boilerplate Start
+require 'pry'
+
+class String
+  def colorize(color_code); "\e[#{color_code}m#{self}\e[0m"; end
+  def red; colorize(31); end
+  def green; colorize(32); end
+  def yellow; colorize(33); end
+  def blue; colorize(34); end
+end
+
+module Loggable
+  attr_accessor :logging, :log_depth
+
+  def log(s, *args);
+    @log_depth ||= "  "
+    out = nil
+
+    puts "#{@log_depth}#{s} #{args.map(&:to_s).join(", ")}" if @logging;
+
+    if block_given?
+      original_depth = @log_depth
+      @log_depth = @log_depth + "  "
+      begin
+        out = yield
+      ensure
+        @log_depth = original_depth
+      end
+    end
+
+    return out
+  end
+end
+
+class Boilerstate
+  include Loggable
+  attr_accessor :input, :options
+  def initialize(input, **options)
+    @options = options || {}
+    @logging = @options[:logging] || false
+    parse(input)
+  end
+
+  def parse(input)
+    raise NotImplementedError
+  end
+end
+
+def value_string(val)
+  value_str = val
+  value_str = '"' + value_str + '"' if val.is_a?(String)
+  value_str = 'nil' if val.nil?
+  value_str
+end
+
+def assert_equal(expect, value, description)
+  puts "  #{'✔'.green} #{description} == #{value_string(value)}" if value == expect
+  puts "  #{'✖'.red} #{description}: expected #{value_string(expect)}, received #{value_string(value)}" if value != expect
+  value
+end
+
+def assert_call(expect, *args)
+  assert_call_on(self, expect, *args)
+end
+
+def log_call(method, *args)
+  log_call_on(self, method, *args)
+end
+
+def assert_call_on(target, expect, *args)
+  log_call_on(target, *args) {|m, r, desc| assert_equal(expect, r, "#{m}(#{desc})")}
+end
+
+def log_call_on(target, method, *args)
+  arg_description = args.to_s[1...-1]
+  arg_description = "..." if arg_description.length > 10
+  method, arg_description = method if method.is_a?(Array)
+
+  result = target.send(method, *args)
+  return yield method, result, arg_description if block_given?
+  puts "  #{'-'.yellow} #{method}(#{arg_description}) == #{value_string(result)}"
+end
+
+def input
+  if ARGV.length == 0
+    raise "no input provided, please specify file as command line arg"
+  end
+  @input ||= $<.map(&:to_s)
+  @input.length == 1 ? @input[0].dup : @input.dup # prevents alterations to source
+end
+
+def part(num, &block)
+  puts "Part #{num.to_s}:".green; yield; puts ""
+end
+# Advent Boilerplate End
